cfb054e83696431a8d9527b59693d4e90f371dd3
Mervan Kilic <merwnk@MacBook-Air-de-Mervan.local>
2024-10-31 15:41:31 +0100

    caca

--- scene/scene.h
+++ scene/scene.h
@@ -9,11 +9,35 @@ typedef struct s_list {
     void    *next;
 }               t_list;
 
-typedef struct s_scene {
+typedef struct s_am_ligth {
 
         t_color color;
-        float   ratio;
-}               t_scene;
+        double   ratio;
+}               t_am_light;
+
+typedef struct s_cam {
+
+        double  fov;
+        double  x;
+        double  y;
+        double  z;
+        t_vec   dir;
+}       t_cam;
+
+typedef struct s_light 
+{
+        double  x;
+        double  y;
+        double  z;
+        double  ratio;
+        t_color color;
+}               t_light;
+
+typedef struct s_scene
+{
+        
+};
+
 
 
 #endif

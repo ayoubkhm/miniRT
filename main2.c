c24a1e238447ab5fe976976e15aab17226a3da45
Mervan Kilic <merwnk@Air-de-Mervan.lan>
2024-11-07 21:21:20 +0100

    un peu plus de visu

--- main2.c
+++ main2.c
@@ -9,31 +9,10 @@
 #define WIDTH 800
 #define HEIGHT 600
 
-void close_window(t_scene *scene) {
+void close_window(t_scene *scene) 
+{
     mlx_destroy_window(scene->mlx, scene->win);
     exit(0);
-}
-
-int key_press(int keycode, t_scene *scene) {
-    if (keycode == 53) { // Touche Échap (code 53 sur Mac)
-        close_window(scene);
-    }
-    // Mouvements WASD (ajuster les codes selon ton OS si nécessaire)
-    else if (keycode == 13) { // W - Avancer
-        scene->camera.position.z += 1;
-    }
-    else if (keycode == 1) { // S - Reculer
-        scene->camera.position.z -= 1;
-    }
-    else if (keycode == 0) { // A - Gauche
-        scene->camera.position.x -= 1;
-    }
-    else if (keycode == 2) { // D - Droite
-        scene->camera.position.x += 1;
-    }
-    // Redessiner la scène après chaque mouvement
-    render_scene(scene);
-    return (0);
 }
 
 t_vec compute_ray(int i, int j, t_cam camera) {
@@ -65,7 +44,7 @@ t_vec compute_ray(int i, int j, t_cam camera) {
     return ray_direction;
 }
 
-void render_scene(t_scene *scene, t_cylinder cyl, t_plane pl, void *mlx, void *win)
+void render_scene(t_scene *scene, t_plane pl)
 {
         int     i = 0;
         int     j = 0;
@@ -78,16 +57,16 @@ void render_scene(t_scene *scene, t_cylinder cyl, t_plane pl, void *mlx, void *w
                 {
                         t_vec ray_dir = compute_ray(i, j, scene->camera);
 
-                        double t_plane, t_cylinder;
+                        double t_plane;
                         bool hit_plane = intersect_plane(scene->camera.position, ray_dir, &pl, &t_plane);
-                        bool hit_cylinder = intersect_cylinder(scene->camera.position, ray_dir, cyl, &t_cylinder);
+                        // bool hit_cylinder = intersect_cylinder(scene->camera.position, ray_dir, cyl, &t_cylinder);
                         // bool hit_sphere = intersect_sphere(scene->camera.position, ray_dir, sp, &t_sphere);
 
                         // if (hit_plane > 0 && hit_cylinder > 0) printf("\nplane : %d cylindre : %d\n", hit_plane > 0, hit_cylinder > 0);
 
                         t_color color = {255, 255, 255}; // Fond blanc par défaut ?¿
 
-                        if (hit_plane && (!hit_cylinder || t_plane < t_cylinder))
+                        if (hit_plane)
                         {
                                 if (pl.flag_qud)
                                 {
@@ -97,14 +76,34 @@ void render_scene(t_scene *scene, t_cylinder cyl, t_plane pl, void *mlx, void *w
                                 else
                                         color = pl.color;
                         }
-                        else if (hit_cylinder)
-                                color = cyl.color;
                         col = color.b + (color.g << 8) + (color.r << 16);
-                        mlx_pixel_put(mlx, win, i, j, col);
+                        mlx_pixel_put(scene->mlx, scene->win, i, j, col);
                         i++;
                 }
                 j++;
         }
+}
+
+int key_press(int keycode, t_scene *scene, t_plane pl) {
+    if (keycode == 53) { // Touche Échap (code 53 sur Mac)
+        close_window(scene);
+    }
+    // Mouvements WASD (ajuster les codes selon ton OS si nécessaire)
+    else if (keycode == 87) { // W - Avancer
+        scene->camera.position.z += 1;
+    }
+    else if (keycode == 1) { // S - Reculer
+        scene->camera.position.z -= 1;
+    }
+    else if (keycode == 0) { // A - Gauche
+        scene->camera.position.x -= 1;
+    }
+    else if (keycode == 2) { // D - Droite
+        scene->camera.position.x += 1;
+    }
+    // Redessiner la scène après chaque mouvement
+        render_scene(scene, pl);
+    return (0);
 }
 
 
@@ -148,10 +147,11 @@ int main(int argc, char **argv)
         // mlx = mlx_init();
         // win = mlx_new_window(mlx, WIDTH, HEIGHT, "Cylindre et plan");
 
-        render_scene(&scene, cyl, pl, mlx, win);
+        render_scene(&scene, pl);
+        mlx_key_hook(scene.win, key_press, &scene);
 
 
-        mlx_loop(mlx);
+        mlx_loop(scene.mlx);
         free_scene(&scene);
         return (EXIT_SUCCESS);
 }

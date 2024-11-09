58950eb86ead4534c3fd18d01e21ab9057648fc0
Debian <debian@ov-7cb7be>
2024-11-09 08:11:53 +0100

    removed global variable + corrected maths for cylinder

--- main2.c
+++ main2.c
@@ -20,15 +20,13 @@ t_vec compute_ray(int i, int j, t_cam camera) {
     float viewport_height = 2.0 * tan(fov_rad / 2.0);
     float viewport_width = viewport_height * aspect_ratio;
     
-    t_vec up = {0, 1, 0}; // Vecteur "up" du monde
+    t_vec up = {0, 1, 0}; 
     t_vec camera_direction = vector_normalize(camera.direction);
     
-    // Calcul des vecteurs de base de la caméra
     t_vec camera_right = vector_normalize(vector_cross(camera_direction, up));
-    t_vec camera_up = vector_cross(camera_right, camera_direction); // Attention à l'ordre des arguments
+    t_vec camera_up = vector_cross(camera_right, camera_direction);
     
-    // Calcul des coordonnées x et y dans l'espace de la caméra
-    float pixel_ndc_x = (i + 0.5) / (float)WIDTH;  // Normalized Device Coordinate
+    float pixel_ndc_x = (i + 0.5) / (float)WIDTH;
     float pixel_ndc_y = (j + 0.5) / (float)HEIGHT;
     float pixel_screen_x = 2 * pixel_ndc_x - 1;
     float pixel_screen_y = 1 - 2 * pixel_ndc_y;
@@ -36,20 +34,18 @@ t_vec compute_ray(int i, int j, t_cam camera) {
     float pixel_camera_x = pixel_screen_x * viewport_width / 2.0;
     float pixel_camera_y = pixel_screen_y * viewport_height / 2.0;
     
-    // Direction du rayon dans l'espace caméra
     t_vec ray_direction = vector_normalize(
         vector_add(
             vector_add(
                 scale_vec(camera_right, pixel_camera_x),
                 scale_vec(camera_up, pixel_camera_y)
             ),
-            camera_direction // Ajoute la direction de la caméra
+            camera_direction
         )
     );
     
     return ray_direction;
 }
-
 
 void render_scene(t_scene *scene)
 {
@@ -64,42 +60,32 @@ void render_scene(t_scene *scene)
         return;
     }
 
-    // Parcours des objets de la scène pour vérifier leurs couleurs
     t_list *current = scene->objects;
     while (current) {
         t_object *object = (t_object *)current->content;
-
-        // Affiche les informations de couleur pour chaque type d'objet une seule fois
         if (object->type == PLANE) {
-            printf("Plane color: {R:%d, G:%d, B:%d}\n",
-                   object->color.r, object->color.g, object->color.b);
+            printf("Plane color: {R:%d, G:%d, B:%d}\n", object->color.r, object->color.g, object->color.b);
         } else if (object->type == CYLINDER) {
-            printf("Cylinder color: {R:%d, G:%d, B:%d}\n",
-                   object->color.r, object->color.g, object->color.b);
+            printf("Cylinder color: {R:%d, G:%d, B:%d}\n", object->color.r, object->color.g, object->color.b);
         } else if (object->type == SPHERE) {
-            printf("Sphere color: {R:%d, G:%d, B:%d}\n",
-                   object->color.r, object->color.g, object->color.b);
+            printf("Sphere color: {R:%d, G:%d, B:%d}\n", object->color.r, object->color.g, object->color.b);
         }
-
         current = current->next;
     }
 
-    // Boucle pour chaque pixel de l'écran
     while (j < HEIGHT) {
         i = 0;
         while (i < WIDTH) {
             t_vec ray_dir = compute_ray(i, j, scene->camera);
-            t_color color = {255, 255, 255}; // Fond blanc par défaut
-            double t_min = INFINITY; // Distance minimale pour l'objet le plus proche
+            t_color color = {255, 255, 255};
+            double t_min = INFINITY;
 
-            // Parcours des objets de la scène pour le rendu
             current = scene->objects;
             while (current) {
                 t_object *object = (t_object *)current->content;
-                double t = INFINITY; // Distance d'intersection temporaire
-                bool hit = false;    // Indicateur de collision
+                double t = INFINITY;
+                bool hit = false;
 
-                // Vérification de l'intersection en fonction du type d'objet
                 if (object->type == PLANE) {
                     hit = intersect_plane(scene->camera.position, ray_dir, (t_plane *)object->data, &t);
                 } else if (object->type == CYLINDER) {
@@ -108,19 +94,16 @@ void render_scene(t_scene *scene)
                     hit = intersect_sphere(scene->camera.position, ray_dir, *(t_sphere *)object->data, &t);
                 }
 
-                // Si une intersection est trouvée et qu'elle est plus proche, mettre à jour `color`
                 if (hit && t < t_min && t > 0) {
                     t_min = t;
-                    color = object->color; // Utilise la couleur de l'objet
+                    color = object->color;
                 }
 
                 current = current->next;
             }
 
-            // Conversion de la couleur en format RGB pour `mlx_pixel_put`
             col = color.b + (color.g << 8) + (color.r << 16);
 
-            // Dessin du pixel
             if (scene->mlx && scene->win) {
                 mlx_pixel_put(scene->mlx, scene->win, i, j, col);
             }
@@ -133,32 +116,21 @@ void render_scene(t_scene *scene)
     printf("Render completed.\n");
 }
 
-
-
-
-
-
-
-
-
-
-
 int key_press(int keycode, t_scene *scene) {
-    if (keycode == 53) { // Touche Échap (code 53 sur Mac)
+    if (keycode == 53) {
         close_window(scene);
-    } else if (keycode == 87) { // W - Avancer
+    } else if (keycode == 87) {
         scene->camera.position.z += 1;
-    } else if (keycode == 1) { // S - Reculer
+    } else if (keycode == 1) {
         scene->camera.position.z -= 1;
-    } else if (keycode == 0) { // A - Gauche
+    } else if (keycode == 0) {
         scene->camera.position.x -= 1;
-    } else if (keycode == 2) { // D - Droite
+    } else if (keycode == 2) {
         scene->camera.position.x += 1;
     }
-    render_scene(scene); // Appel avec un seul argument
+    render_scene(scene);
     return (0);
 }
-
 
 int main(int argc, char **argv)
 {
@@ -169,10 +141,9 @@ int main(int argc, char **argv)
 
     printf("Initialisation\n");
 
-    // Initialisation de la scène
+    t_scene scene;
     init_scene(&scene);
 
-    // Chargement de la scène à partir du fichier
     if (!load_scene(&scene, argv[1])) {
         printf("Erreur lors du chargement de la scène.\n");
         free_scene(&scene);
@@ -180,14 +151,11 @@ int main(int argc, char **argv)
     }
 
     printf("Scene loaded successfully:\n");
-    // Rendu de la scène
     render_scene(&scene);
 
-    // Gestion des événements clavier et boucle MLX
     mlx_key_hook(scene.win, key_press, &scene);
     mlx_loop(scene.mlx);
 
-    // Libération des ressources de la scène
     free_scene(&scene);
     return (EXIT_SUCCESS);
 }

22f3cdcf7b8c8b0f5b0a8e14d3691d4c293e095f
Mervan Kilic <merwnk@Air-de-Mervan.lan>
2024-11-10 04:15:43 +0100

    Nocturne

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include "includes/minirt.h"
 
+void    print_position_or_vect(t_vec vec, char *name)
+{
+    printf("\n%s :\nx = %.1f\ny = %.1f\nz = %.1f\n", name, vec.x, vec.y, vec.z);
+
+}
+
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -18,10 +25,13 @@ int main(int argc, char **argv)
     }
     //printf("Scene loaded successfully:\n");
     render_scene(&scene);
-    mlx_hook(scene.win, 2, 1L << 0, key_press, &scene);     // 2 est le code d'un keypress dans MiniLibX
-    mlx_hook(scene.win, 3, 1L << 1, key_release, &scene);   // 3 est le code d'un keyrelease dans MiniLibX
-    mlx_loop_hook(scene.mlx, (void *)update_camera_position, &scene);
+    // mlx_hook(scene.win, 2, 1L << 0, key_press, &scene);     // 2 est le code d'un keypress dans MiniLibX
+    // mlx_hook(scene.win, 3, 1L << 1, key_release, &scene);   // 3 est le code d'un keyrelease dans MiniLibX
+    // mlx_loop_hook(scene.mlx, (void *)update_camera_position, &scene);
     mlx_loop(scene.mlx);
+    t_light lumiere = *(t_light *)scene.lights->content;
+    print_position_or_vect(lumiere.position, "Light");
+    print_position_or_vect(scene.camera.position, "Camera");
     free_scene(&scene);
     return (EXIT_SUCCESS);
 }

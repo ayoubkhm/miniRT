412e1306c808047cac07abf834cd1d006750f571
Mervan Kilic <mervkili@ov-7cb7be>
2025-03-08 20:10:27 +0100

    libft

--- lib/libft/ft_atof.c
+++ lib/libft/ft_atof.c
@@ -10,32 +10,76 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-double	ft_atof(const char *str)
+#include "libft.h"
+
+static void	skip_whitespace(const char *str, int *i)
+{
+	while (str[*i] == ' ' || str[*i] == '\t' || str[*i] == '\n')
+		(*i)++;
+}
+
+static int	parse_sign(const char *str, int *i)
+{
+	int	sign;
+
+	sign = 1;
+	if (str[*i] == '-')
+	{
+		sign = -1;
+		(*i)++;
+	}
+	else if (str[*i] == '+')
+	{
+		sign = 1;
+		(*i)++;
+	}
+	return (sign);
+}
+
+static double	parse_integer(const char *str, int *i)
 {
 	double	result;
-	double	fraction;
-	int		sign;
-	int		i;
 
 	result = 0.0;
-	fraction = 0.0;
-	sign = 1;
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-		sign = (str[i++] == '-') ? -1 : 1;
-	while (str[i] && str[i] >= '0' && str[i] <= '9')
-		result = result * 10 + (str[i++] - '0');
-	if (str[i] == '.')
+	while (str[*i] && str[*i] >= '0' && str[*i] <= '9')
 	{
-		i++;
-		double divisor = 10;
-		while (str[i] && str[i] >= '0' && str[i] <= '9')
+		result = result * 10 + (str[*i] - '0');
+		(*i)++;
+	}
+	return (result);
+}
+
+static double	parse_fraction(const char *str, int *i)
+{
+	double	fraction;
+	double	divisor;
+
+	fraction = 0.0;
+	divisor = 10.0;
+	if (str[*i] == '.')
+	{
+		(*i)++;
+		while (str[*i] && str[*i] >= '0' && str[*i] <= '9')
 		{
-			fraction += (str[i++] - '0') / divisor;
-			divisor *= 10;
+			fraction += (str[*i] - '0') / divisor;
+			divisor *= 10.0;
+			(*i)++;
 		}
 	}
-	return sign * (result + fraction);
+	return (fraction);
+}
+
+double	ft_atof(const char *str)
+{
+	int		i;
+	double	integer_part;
+	double	fraction_part;
+	int		sign;
+
+	i = 0;
+	skip_whitespace(str, &i);
+	sign = parse_sign(str, &i);
+	integer_part = parse_integer(str, &i);
+	fraction_part = parse_fraction(str, &i);
+	return (sign * (integer_part + fraction_part));
 }
